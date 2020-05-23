e8e01c219eb525f2f9816ec225c55ef5a305ebb9
Sriramnark <64322927+rambo-94@users.noreply.github.com>
2020-05-23 18:02:16 -0500

    ++ Configuartion for connecting to MYSQL

--- pet-clinic-data/src/main/java/com/springboot/petclinic/JPAConfiguration.java
+++ pet-clinic-data/src/main/java/com/springboot/petclinic/JPAConfiguration.java
@@ -1,35 +1,37 @@
-//package com.springboot.petclinic;
-//
-//import org.springframework.boot.jdbc.DataSourceBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//
-//import javax.sql.DataSource;
-//
-//
-//@Configuration
-//public class JPAConfiguration {
-//
-//    @Bean(name = "h2DataSource")
-//    public DataSource h2DataSource()
-//    {
-//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-//        dataSourceBuilder.driverClassName("org.h2.Driver");
-//        dataSourceBuilder.url("jdbc:h2:file:C:/temp/test");
-//        dataSourceBuilder.username("sa");
-//        dataSourceBuilder.password("");
-//        return dataSourceBuilder.build();
-//    }
-//
-//    @Bean(name = "mySqlDataSource")
-//    @Primary
-//    public DataSource mySqlDataSource()
-//    {
-//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-//        dataSourceBuilder.url("jdbc:mysql://localhost/testdb");
-//        dataSourceBuilder.username("dbuser");
-//        dataSourceBuilder.password("dbpass");
-//        return dataSourceBuilder.build();
-//    }
-//}
+package com.springboot.petclinic;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class JPAConfiguration {
+
+    @Bean(name = "h2DataSource")
+    public DataSource h2DataSource()
+    {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:file:C:/temp/test");
+        dataSourceBuilder.username("sa");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
+
+    @Bean(name = "mySqlDataSource")
+    @Primary
+    public DataSource mySqlDataSource()
+    {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("com.mysql.jdbc.Driver");
+        dataSourceBuilder.url("jdbc:mysql://localhost/pet-clinic-db");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("Ramkanth@1");
+
+        return dataSourceBuilder.build();
+    }
+}
