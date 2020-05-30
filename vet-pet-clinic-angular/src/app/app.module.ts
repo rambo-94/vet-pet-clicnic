b2344c448128677227401637cd5bb69f838e9860
Sriramnark <64322927+rambo-94@users.noreply.github.com>
2020-05-30 14:19:36 -0500

    Implemented feature to add pet in UI

--- vet-pet-clinic-angular/src/app/app.module.ts
+++ vet-pet-clinic-angular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {OwnersModule} from './owners/owners.module';
 
 import { AppComponent } from './app.component';
 import {PetsModule} from './pets/pets.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -14,7 +15,8 @@ import {PetsModule} from './pets/pets.module';
   imports: [
     BrowserModule,
     PetsModule,
-    OwnersModule
+    OwnersModule,
+    BrowserAnimationsModule
 
   ],
   providers: [],
--- vet-pet-clinic-angular/src/app/pets/pet-add/pet-add.component.ts
+++ vet-pet-clinic-angular/src/app/pets/pet-add/pet-add.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Pet} from '../pet';
+import {OwnerService} from '../../owners/owner.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Owner} from '../../owners/owner';
+import {PetType} from '../../pettypes/pettype';
+import {PetService} from '../pet.service';
+import {PetTypeService} from '../../pettypes/pet.service';
 
 @Component({
   selector: 'app-pet-add',
@@ -7,9 +14,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PetAddComponent implements OnInit {
 
-  constructor() { }
+  pet: Pet;
+  @Input() currentType: PetType;
+  currentOwner: Owner;
+  petTypes: PetType[];
+  addedSuccess = false;
+  errorMessage: string;
+
+  constructor(private ownerService: OwnerService, private petService: PetService,
+              private petTypeService: PetTypeService, private router: Router, private route: ActivatedRoute) {
+    this.pet = {} as Pet;
+    this.currentOwner = {} as Owner;
+    this.currentType = {} as PetType;
+    this.petTypes = [];
+  }
 
   ngOnInit() {
+    this.petTypeService.getPetTypes().subscribe(
+      pettypes => this.petTypes = pettypes,
+      error => this.errorMessage = error as any);
+
+    const ownerId = this.route.snapshot.params.id;
+    this.ownerService.getOwnerById(ownerId).subscribe(
+      response => {
+        this.currentOwner = response;
+      },
+      error => this.errorMessage = error as any);
+  }
+
+  onSubmit(pet: Pet) {
+    pet.id = null;
+    pet.owner = this.currentOwner;
+    // format output from datepicker to short string yyyy/mm/dd format
+    this.petService.addPet(pet).subscribe(
+      newPet => {
+        this.pet = newPet;
+        this.addedSuccess = true;
+        this.gotoOwnerDetail();
+      },
+      error => this.errorMessage = error as any);
+  }
+
+  gotoOwnerDetail() {
+    this.router.navigate(['/owners', this.currentOwner.id]);
   }
 
 }
