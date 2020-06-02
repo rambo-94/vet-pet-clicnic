26ec09d395222a89e61f48df895e5bced287958a
Sriramnark <64322927+rambo-94@users.noreply.github.com>
2020-06-02 13:09:45 -0500

    Implemnted routing of views and services for pet module

--- vet-pet-clinic-angular/src/app/pets/pet-edit/pet-edit.component.ts
+++ vet-pet-clinic-angular/src/app/pets/pet-edit/pet-edit.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Pet} from '../pet';
+import {Owner} from '../../owners/owner';
+import {ActivatedRoute, Router} from '@angular/router';
+import {PetService} from '../pet.service';
+import {PetType} from '../../pettypes/pettype';
+import {PetTypeService} from '../../pettypes/pet.service';
 
 @Component({
   selector: 'app-pet-edit',
@@ -7,9 +13,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PetEditComponent implements OnInit {
 
-  constructor() { }
+  pet: Pet;
+  @Input() currentType: PetType;
+  currentOwner: Owner;
+  petTypes: PetType[];
+  errorMessage: string;
 
-  ngOnInit() {
+  constructor(private petService: PetService, private petTypeService: PetTypeService, private router: Router,
+              private route: ActivatedRoute) {
+    this.pet = {} as Pet;
+    this.currentOwner = {} as Owner;
+    this.currentType = {} as PetType;
+    this.petTypes = [];
   }
 
+  ngOnInit() {
+
+    this.petTypeService.getPetTypes().subscribe(
+      pettypes => this.petTypes = pettypes,
+      error => this.errorMessage = error as any);
+
+    const petId = this.route.snapshot.params.id;
+    this.petService.getPetById(petId).subscribe(
+      pet => {
+        this.pet = pet;
+        this.currentOwner = this.pet.owner;
+        this.currentType = this.pet.petType;
+      },
+      error => this.errorMessage = error as any);
+
+  }
+
+  onSubmit(pet: Pet) {
+    pet.petType = this.currentType;
+    const that = this;
+    // format output from datepicker to short string yyyy/mm/dd format
+
+
+    this.petService.updatePet(pet.id.toString(), pet).subscribe(
+      res => this.gotoOwnerDetail(this.currentOwner),
+      error => this.errorMessage = error as any
+    );
+  }
+
+  gotoOwnerDetail(owner: Owner) {
+    this.router.navigate(['/owners', owner.id]);
+  }
 }
--- vet-pet-clinic-angular/src/app/pets/pets.module.ts
+++ vet-pet-clinic-angular/src/app/pets/pets.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 import { PetAddComponent } from './pet-add/pet-add.component';
 import { PetEditComponent } from './pet-edit/pet-edit.component';
 import { PetListComponent } from './pet-list/pet-list.component';
-
+import {FormsModule} from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material';
+import {PetsRoutingModule} from './pets-routing.module';
+import {PetService} from './pet.service';
 
 
 @NgModule({
   declarations: [PetAddComponent, PetEditComponent, PetListComponent],
+  exports: [
+    PetListComponent,
+
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    MatDatepickerModule,
+    PetsRoutingModule
+
+  ],
+  providers: [PetService]
 })
 export class PetsModule { }
